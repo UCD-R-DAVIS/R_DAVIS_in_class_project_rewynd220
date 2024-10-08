ec4569a59fbeb220248150682ca441df6db7a684
Rebecca Wynd <rebeccawynd@Rebeccas-Air-2.attlocal.net>
2024-10-08 11:40:32 -0700

    final hw submission

--- scripts/week_2_homework.R
+++ scripts/week_2_homework.R
@@ -3,16 +3,23 @@ hw2 <- runif(50, 4, 50)
 hw2 <- replace(hw2, c(4,12,22,27), NA)
 hw2
 
-?set.seed
-?runif
-?replace
+#remove all NAs 
+hw2[!is.na(hw2)]
+hw2<-hw2[!is.na(hw2)]
 hw2
-hw2[NA]
-?subset
-#remove NA
-hw2[!hw2 %in% c(NA)]
-#rename hw2 without NAs
-hw2<-hw2[!hw2 %in% c(NA)]
-hw2
-#select range of values 
 
+#select all numbers between 14 and 38 inclusive
+hw2[hw2>=14 & hw2<=38]
+prob1 <-hw2[hw2>=14 & hw2<=38]
+prob1
+
+#multiply each number in prob1 vector by 3 
+times3<-prob1*3
+times3
+
+#add 10
+plus10<-times3+10
+plus10
+
+#select every other number in plus10 vector 
+plus10[c(TRUE,FALSE)]
