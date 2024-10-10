fb80cf1493020d2f1abfa9faa3a57bb07de295af
Rebecca Wynd <rebeccawynd@campus-027-231.ucdavis.edu>
2024-10-10 13:43:15 -0700

    notes from class added

--- scripts/week_2_homework.R
+++ scripts/week_2_homework.R
@@ -7,11 +7,17 @@ hw2
 hw2[!is.na(hw2)]
 hw2<-hw2[!is.na(hw2)]
 hw2
+#when would it make sense to use na.omit vs !is.na ? different na functions can behave differently
+#also can use hw2[complete.cases(hw2)] #takes data that only has complete observations 
+#to remove a certain number like 10, you would hw2[!hw2does not equal 10] (something like that)
+
 
 #select all numbers between 14 and 38 inclusive
 hw2[hw2>=14 & hw2<=38]
 prob1 <-hw2[hw2>=14 & hw2<=38]
 prob1
+#or would be a |
+#hw2[c14:38] is referring to index value 14 - 38 not the numbers in the vector 
 
 #multiply each number in prob1 vector by 3 
 times3<-prob1*3
@@ -23,3 +29,7 @@ plus10
 
 #select every other number in plus10 vector 
 plus10[c(TRUE,FALSE)]
+#says it wants first number, not second number, and recycles 
+
+#if you have to do math with a vector with NAs in it 
+mean(hw.2,na.rm=TRUE)
