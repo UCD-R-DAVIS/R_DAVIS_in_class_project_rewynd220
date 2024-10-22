09cdb5f6474dec7484d0e9d5f312c0522aea5b2d
Rebecca Wynd <rebeccawynd@Rebeccas-Air-2.attlocal.net>
2024-10-22 12:36:26 -0700

    HW start

---
+++ scripts/Week4_homework.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+#Create a tibble named surveys from the portal_data_joined.csv file.
+surveys <- read_csv('data/portal_data_joined.csv')
+#Subset surveys using Tidyverse methods to keep rows with weight between 30 and 60, and print out the first 6 rows.
+weight_range <- surveys %>% 
+  filter(weight >= 30 & weight <= 60)
+head(weight_range, n=6)
+#Create a new tibble showing the maximum weight for each species + sex combination and name it biggest_critters. Sort the tibble to take a look at the biggest and smallest species + sex combinations. HINT: it’s easier to calculate max if there are no NAs in the dataframe…
+biggest_critters <- surveys %>% 
+  filter(!is.na(weight)) %>% 
+  group_by(sex) %>% 
+  summarise(max(weight), min(weight)) 
+#Try to figure out where the NA weights are concentrated in the data- is there a particular species, taxa, plot, or whatever, where there are lots of NA values? There isn’t necessarily a right or wrong answer here, but manipulate surveys a few different ways to explore this. Maybe use tally and arrange here.
+NA_weights <- surveys %>% 
+  filter(is.na(weight)) %>% 
+  arrange(species_id)
+
+#Take surveys, remove the rows where weight is NA and add a column that contains the average weight of each species+sex combination to the full surveys dataframe. Then get rid of all the columns except for species, sex, weight, and your new average weight column. Save this tibble as surveys_avg_weight.
+
+surveys<- surveys %>% 
+  filter(!is.na(weight)) %>% 
+  group_by(species_id,sex) %>% 
+  mutate(avg_weight = mean(weight))
+  #why is this what worked? 
+
+surveys_avg_weight <- surveys
+  select(.data = species, sex, weight, avg_weight)
+  
